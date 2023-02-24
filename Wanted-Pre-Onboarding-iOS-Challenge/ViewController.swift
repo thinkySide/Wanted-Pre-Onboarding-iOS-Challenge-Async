428d40540ef2fc23933eb762e77aafa5f42433d0
thinkySide <eunlyuing@gmail.com>
2023-02-24 15:35:25 +0900

    [Feat] 현재 클릭한 UIButton이 속해있는 UIStackView의 Index 출력

--- Wanted-Pre-Onboarding-iOS-Challenge/ViewController.swift
+++ Wanted-Pre-Onboarding-iOS-Challenge/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     // MARK: - IBOutlet
     
     
@@ -20,12 +20,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
-
+    
     
     
     // MARK: - IBAction
     
     @IBAction func loadButtonTapped(_ sender: UIButton) {
+        
+        // 컴포넌트 잡기
+        guard
+            let stackView = sender.superview as? UIStackView,
+            let superStackView = stackView.superview as? UIStackView,
+            let imageView = stackView.arrangedSubviews[0] as? UIImageView
+        else { return }
+        
+        // 내가 클릭한 StackView의 Index 구하기
+        let stackViewList = superStackView.arrangedSubviews
+        for (index, stackView) in stackViewList.enumerated() {
+            if let clickedStackView = stackView as? UIStackView, clickedStackView.arrangedSubviews[2] == sender {
+                print("Clicked index: \(index)")
+                break
+            }
+        }
+        
+        imageView.backgroundColor = .systemRed
+        
+        
+        
         // 1. 서버 통신 (비동기)
         // 2. 이미지 표시
     }
