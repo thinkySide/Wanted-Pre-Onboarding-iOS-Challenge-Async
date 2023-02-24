a520925c76ac9ad06aeb9aabf4fffa3b9828fe10
thinkySide <eunlyuing@gmail.com>
2023-02-24 17:45:52 +0900

    [Fix]  URLSession 추가
    - 기존에 URLSession 없이 진행하다 보니 오류 났었음.
    - image 업데이트를 위한 URLSession을 이용한 업데이트 기능 수정

--- Wanted-Pre-Onboarding-iOS-Challenge/ViewController.swift
+++ Wanted-Pre-Onboarding-iOS-Challenge/ViewController.swift
@@ -12,30 +12,22 @@ class ViewController: UIViewController {
     // MARK: - Variable
     var currentIndex = 0
     let imageURL = [
-        "https://user-images.githubusercontent.com/113565086/221117769-7bd8f46a-965f-4674-b430-f4e5ef6c6105.png",
-        "https://user-images.githubusercontent.com/113565086/221118961-5004600b-b0ea-4783-86fd-be3d9fd18502.png",
-        "https://user-images.githubusercontent.com/113565086/221119071-11e8886c-fe8e-49cb-b88d-1be8be81fe77.png",
-        "https://user-images.githubusercontent.com/113565086/221119002-33eab713-c594-4840-a774-f2d87e8b20cb.png",
-        "https://user-images.githubusercontent.com/113565086/221119144-f2a4e119-921e-45ae-b938-c0a36ae30e44.png"
+        "https://drive.google.com/uc?export=download&id=1F8orTckEM8vRhrqDQZu3Ycsde9H5BA7A",
+        "https://drive.google.com/uc?export=download&id=15GktXfKtJuJl-nJeKPExcf11QlIlHCXA",
+        "https://drive.google.com/uc?export=download&id=1FstKka8HKQQuM_YgOv5LCVVWMANCVsTm",
+        "https://drive.google.com/uc?export=download&id=10DaqhTjlC9bW7VMgQQXBDGRJ-giGsvPf",
+        "https://drive.google.com/uc?export=download&id=1YCxh8tYfYAwrRJbILzTotxHotEOO3pHh"
     ]
     
     
-    // MARK: - IBOutlet
-    
-    
-    
-    
     // MARK: - ViewController LifeCycle
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
     
-    
     // MARK: - IBAction
-    
     @IBAction func loadButtonTapped(_ sender: UIButton) {
         
         // 컴포넌트 잡기
@@ -45,6 +37,9 @@ class ViewController: UIViewController {
             let imageView = stackView.arrangedSubviews[0] as? UIImageView
         else { return }
         
+        // 이미지 초기화
+        imageView.image = UIImage(systemName: "photo.fill")
+        
         // 내가 클릭한 StackView의 Index 구하기
         let stackViewList = superStackView.arrangedSubviews
         for (index, stackView) in stackViewList.enumerated() {
@@ -53,35 +48,45 @@ class ViewController: UIViewController {
                 break
             }
         }
-        
         imageUpdate(imageView: imageView)
+    }
 
-    }
-    
-    func imageUpdate(imageView: UIImageView) {
-        
-        // 현재 선택된 index의 이미지 URL
-        guard let url = URL(string: imageURL[currentIndex]) else { return }
-        
-        // 글로벌 큐에서 이미지 불러오기
-        DispatchQueue.global().async {
-            let data = try? Data(contentsOf: url)
-            let image = UIImage(data: data!)
-            
-            // 화면 업데이트는 메인큐에서 진행
-            DispatchQueue.main.async {
-                imageView.image = image
-            }
-            
-        }
-    }
-    
-    
     @IBAction func loadAllImageButtonTapped(_ sender: UIButton) {
         // 1. 서버 통신
         // 2. 모든 UIImage 다운로드 (동시큐에 올리기)
         // 3. 이미지 표시
     }
     
+    
+    // MARK: - Function
+    func imageUpdate(imageView: UIImageView) {
+        
+        // 현재 선택된 index의 이미지 URL
+        guard let url = URL(string: imageURL[currentIndex]) else { return }
+        
+        // URLSession
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            // 에러 확인
+            guard error == nil else {
+                print("에러가 발생했습니다.: \(error!.localizedDescription)")
+                return
+            }
+            
+            // 데이터 확인
+            guard let data = data else {
+                print("데이터를 전달 받지 못했습니다.")
+                return
+            }
+            
+            // image 업데이트
+            DispatchQueue.main.async { // UI업데이트는 main queue에서 진행
+                let image = UIImage(data: data)
+                imageView.image = image
+            }
+            
+        }.resume()
+        
+    }
 }
 
